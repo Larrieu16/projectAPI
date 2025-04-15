05b3c2231a13ef289d4d60b26c1a64140ea0f250
larrieu16 <larrieuestudos@gmail.com>
2025-04-15 09:30:01 -0300

    feat: initial commit

---
+++ projectAPI/src/test/java/io/github/larrieu/projectAPI/ProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.larrieu.projectAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
