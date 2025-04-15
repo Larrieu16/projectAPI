34e47f98d4650ba9ac9f013e0d46ac4d1777d823
larrieu16 <larrieuestudos@gmail.com>
2025-04-15 09:46:18 -0300

    feat: adds "ProductController" controller

---
+++ projectAPI/src/main/java/io/github/larrieu/projectAPI/controller/ProductController.java
@@ -0,0 +1,19 @@
+package io.github.larrieu.projectAPI.controller;
+
+import io.github.larrieu.projectAPI.model.Product;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("products")
+public class ProductController {
+
+    @PostMapping
+    public Product save(@RequestBody Product product) {
+        System.out.println("Product received: " + product);
+        return product;
+    }
+
+}
